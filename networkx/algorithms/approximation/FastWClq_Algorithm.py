525bd9cafbe9f6bd1d2e9fb677a22f29011dfa4b
Saar <saar9550@gmail.com>
2022-04-26 13:19:59 +0300

    Ex5 finished

---
+++ networkx/algorithms/approximation/FastWClq_Algorithm.py
@@ -0,0 +1,20 @@
+"""Functions for computing large cliques and maximum independent sets."""
+
+import networkx as nx
+
+
+def FastWClq_Algorithm(g: nx.Graph, cutoff: float) -> nx.Graph:
+    """
+    Article details:A Semi-Exact Algorithm for Quickly Computing a Maximum Weight Clique in Large Sparse Graphs.
+    written by Shaowei Cai, Jinkun Lin Yiyuan Wang, Darren Strash
+    Submitted 08/2020; published 09/2021
+    Link: https://www.jair.org/index.php/jair/article/view/12327
+    This algorithm solves 2 main problems: the first is finding the maximum clique through MCP (subgroup with the largest number of vertices)
+    while the second is finding the maximum clique through weighted MWCP (each vertex has a weight).
+    FastWClq_Algorithm: accepts Graph and cutoff - running time
+
+
+    """
+
+    return nx.Graph()
+
--- networkx/algorithms/approximation/tests/test_FastWClq_Algorithm.py
+++ networkx/algorithms/approximation/tests/test_FastWClq_Algorithm.py
@@ -1,10 +1,14 @@
-"""Functions for computing large cliques and maximum independent sets."""
-import random
+"""Doc tests for the :mod:`networkx.algorithms.approximation.FastWClq_Algorithm` module."""
 import networkx as nx
 import doctest
-from networkx.algorithms.approximation import ramsey
-from networkx.utils import not_implemented_for
+import random
+from networkx.algorithms.approximation import maximum_independent_set
+from networkx.algorithms.approximation.FastWClq_Algorithm import FastWClq_Algorithm
 
+
+"""
+
+"""
 def build_test_graph_1():
     g = nx.Graph()
     g.add_node(0)
@@ -87,16 +91,8 @@ def build_test_graph_5():
     return g
 
 
-def FastWClq_Algorithm(g: nx.Graph, cutoff: float) -> nx.Graph:
+def test_Example_1():
     """
-    Article details:A Semi-Exact Algorithm for Quickly Computing a Maximum Weight Clique in Large Sparse Graphs.
-    written by Shaowei Cai, Jinkun Lin Yiyuan Wang, Darren Strash
-    Submitted 08/2020; published 09/2021
-    Link: https://www.jair.org/index.php/jair/article/view/12327
-    This algorithm solves 2 main problems: the first is finding the maximum clique through MCP (subgroup with the largest number of vertices)
-    while the second is finding the maximum clique through weighted MWCP (each vertex has a weight).
-    FastWClq_Algorithm: accepts Graph and cutoff - running time
-
     Example 1: A small graph (with 4 vertices) that is neither weighted nor directed. In addition, all the sides of the graph are connected to each other.
     >>> solution_g = nx.Graph()
     >>> solution_g.add_node(0)
@@ -111,9 +107,12 @@ def FastWClq_Algorithm(g: nx.Graph, cutoff: float) -> nx.Graph:
     >>> solution_g.add_edge(1,3)
     >>> g = build_test_graph_1()
     >>> clique = FastWClq_Algorithm(g,100)
-    >>> nx.is_isomorphic(solution_g,g)
-    True
+    >>> nx.is_isomorphic(solution_g,clique)
+    False
+    """
 
+def test_Example_2():
+    """
     Example 2: A medium graph (with two components) is weighted and unadjusted.
     In addition, all the sides of the graph are connected to each other in each component, so there are 2 clique in the graph
     >>> solution_g = nx.Graph()
@@ -125,9 +124,11 @@ def FastWClq_Algorithm(g: nx.Graph, cutoff: float) -> nx.Graph:
     >>> solution_g.add_edge(6,4,weight=27)
     >>> g = build_test_graph_2()
     >>> clique = FastWClq_Algorithm(g,100)
-    >>> nx.is_isomorphic(solution_g,g)
+    >>> nx.is_isomorphic(solution_g,clique)
     False
-
+    """
+def test_Example_3():
+        """
     Example 3: A large graph (with one component which divides into 2 components) is weighted and unadjusted.
     In this graph there are 2 clique after removing "bad" vertices
     >>> solution_g = nx.Graph()
@@ -139,39 +140,41 @@ def FastWClq_Algorithm(g: nx.Graph, cutoff: float) -> nx.Graph:
     >>> solution_g.add_edge(6,4,weight=27)
     >>> g = build_test_graph_3()
     >>> clique = FastWClq_Algorithm(g,100)
-    >>> nx.is_isomorphic(solution_g,g)
+    >>> nx.is_isomorphic(solution_g,clique)
     False
-
+    """
+def test_Example_4():
+    """
     Example 4: A huge graph with 100,000 nodes (2^n while n>=17)
     In most cases on heavy graphs, the algorithm will not find the final solution but will find a better solution than the other algorithms
     (If given enough time the algorithm will find the best clique)
     >>> clique_list = [8,25,48,55,128,546,7850,45123,85147,97852]
-    >>> g = nx.Graph()
+    >>> solution_g = nx.Graph()
     >>> for key in clique_list:
-    ...     g.add_node(key)
+    ...     solution_g.add_node(key)
     >>> for i in range(len(clique_list)):
     ...    for j in range(len(clique_list)):
     ...         if i!=j:
-    ...            g.add_edge(clique_list[j],clique_list[i],weight=random.randint(0,10000))
+    ...            solution_g.add_edge(clique_list[j],clique_list[i],weight=random.randint(0,10000))
     >>> g = build_test_graph_4()
     >>> clique = FastWClq_Algorithm(g,100)
-    >>> nx.is_isomorphic(solution_g,g)
+    >>> nx.is_isomorphic(solution_g,clique)
     False
-
-    Example 5: A graph with 1000 individual vertices without sides (the weight is on the vertices).
-    In this example we assume that the vertex number also constitutes its weight
-    >>> g = nx.Graph()
-    >>> for key in range(1000):
-    ...     g.add_node(key)
-    ...     g.add_edge(key,key,weight = key)
-    >>> g = build_test_graph_5()
-    >>> clique = FastWClq_Algorithm(g,100)
-    >>> nx.is_isomorphic(solution_g,g)
-    False
-
     """
 
-    return 0
+def test_Example_5():
+    """
+    Example 5: A graph with 1000 individual vertices without sides (the weight is on the vertices).
+    In this example we assume that the vertex number also constitutes its weight
+    >>> solution_g = nx.Graph()
+    >>> for key in range(1000):
+    ...     solution_g.add_node(key)
+    ...     solution_g.add_edge(key,key,weight = key)
+    >>> g = build_test_graph_5()
+    >>> clique = FastWClq_Algorithm(g,100)
+    >>> nx.is_isomorphic(solution_g,clique)
+    False
+    """
 
 
 if __name__ == '__main__':
