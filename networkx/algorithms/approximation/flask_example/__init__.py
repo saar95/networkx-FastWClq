dada23c0ddff52fded763e0074bbb51b5cc8b617
Saar <saar9550@gmail.com>
2022-06-12 14:57:36 +0300

    Added an option to run the algorithm by Google Sheets

--- networkx/algorithms/approximation/flask_example/__init__.py
+++ networkx/algorithms/approximation/flask_example/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ecf6e975838a2f7bf3c5dbe7d55ebe5b'  ###
 
+from . import run
--- networkx/algorithms/approximation/flask_example/run.py
+++ networkx/algorithms/approximation/flask_example/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from networkx.algorithms.approximation.url_to_graph import clique_to_sheet
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "RandomString"
 message = "WELL DONE !!!"
 
 
