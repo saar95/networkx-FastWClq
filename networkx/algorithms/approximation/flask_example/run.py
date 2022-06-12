069146d334f8217270bf1f5cd18b08d0e00465f1
Saar <saar9550@gmail.com>
2022-06-12 14:46:01 +0300

    fix google sheet

---
+++ networkx/algorithms/approximation/flask_example/run.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from networkx.algorithms.approximation.url_to_graph import clique_to_sheet
+app = Flask(__name__)
+
+message = "WELL DONE !!!"
+
+
+@app.route('/')
+def action():
+    return render_template('layout.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_data():
+    url = request.form.get('url')
+    clique_to_sheet('Input')
+    return message
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
